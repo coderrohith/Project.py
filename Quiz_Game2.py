58e199589b8aa5167ba83e0b71208e7939aff210
coderrohith <rohithrgowda19@gmail.com>
2026-06-19 21:02:16 +0530

    Quiz_Game_program

---
+++ Quiz_Game2.py
@@ -0,0 +1,15 @@
+score=0
+
+print("Python Quiz")
+
+ans=input("1.Who daveloped python ?:")
+
+if ans.lower() == "quido van rossum":
+    score +1
+
+    ans=input("2.Python is a(Prigramming language/Database)?:")
+
+if ans.lower() == "programmig language":
+    score +=1
+
+    print("Your Score =",score)
