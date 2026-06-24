dc78c3df1b20247ea855134ab5a513be9851cc96
coderrohith <rohithrgowda19@gmail.com>
2026-06-24 17:01:05 +0530

    Random_quotes_gen

---
+++ Random_Quote_Generator.py
@@ -0,0 +1,10 @@
+import random
+
+quotes = [
+    "Never give up.",
+    "Success comes from hard work.",
+    "Learn something new everyday.",
+    "Believe in yourself."
+]
+
+print(random.choice(quotes))
