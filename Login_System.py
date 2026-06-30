75b38474c2eb8e437978b33e94855b57b782db5a
coderrohith <rohithrgowda19@gmail.com>
2026-06-30 21:54:45 +0530

    Login_System

---
+++ Login_System.py
@@ -0,0 +1,10 @@
+username="Rohith"
+password="1234"
+
+u=input("Enter Username:")
+p=input("Enter Password:")
+
+if u == username and p == password:
+    print("Login Successfull")
+else:
+    print("Invalid Username or Passwprd")
